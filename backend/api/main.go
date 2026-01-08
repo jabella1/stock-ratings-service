f7621d45c54f3b639796846e5f7a7f03d459dcdc
jabella1 <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2026-01-08 17:52:11 -0500

    feat(stock): add PostgreSQL connection with .env support and integrate stock rating components

---
+++ backend/api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jabella1/stock-ratings-service/internal/app/features/stock/app/query/handler"
+	"github.com/jabella1/stock-ratings-service/internal/app/features/stock/infra/db/postgres" // ← tu paquete
+	"github.com/jabella1/stock-ratings-service/internal/app/features/stock/interface/api/rest"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	connectionString := os.Getenv("CONNECTION_STRING")
+	if connectionString == "" {
+		log.Fatal("CONNECTION_STRING is not set")
+	}
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = ":8080"
+	}
+	context := context.Background()
+	connection, err := postgres.NewConnection(context, connectionString)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer connection.Close(context)
+	queries := postgres.NewQueries(connection)
+	stockRatingRepository := postgres.CreateSqlcStockRatingRepository(queries)
+	stockRatingByTickerQueryHandler := handler.CreateGetStockRatingByTickerQueryHandler(stockRatingRepository)
+
+	e := echo.New()
+	rest.CreateStockRatingController(e, stockRatingByTickerQueryHandler)
+
+	if err := e.Start(port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
