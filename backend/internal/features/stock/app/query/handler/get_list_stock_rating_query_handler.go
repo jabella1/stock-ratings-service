439d3eca23f3a45ececee42330ac8e47b966c056
jabella1 <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2026-01-10 01:31:07 -0500

    feat(handler): add implemented handler for new service

---
+++ backend/internal/features/stock/app/query/handler/get_list_stock_rating_query_handler.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"github.com/jabella1/stock-ratings-service/internal/features/common/pagination"
+	"github.com/jabella1/stock-ratings-service/internal/features/stock/app/dto"
+	"github.com/jabella1/stock-ratings-service/internal/features/stock/app/query"
+	"github.com/jabella1/stock-ratings-service/internal/features/stock/domain/repositories"
+)
+
+type GetListStockRatingQueryHandler struct {
+	stockRatingRepository repositories.StockRatingRepository
+}
+
+func CreateGetListStockRatingQueryHandler(stockRatingRepository repositories.StockRatingRepository) *GetListStockRatingQueryHandler {
+	return &GetListStockRatingQueryHandler{
+		stockRatingRepository: stockRatingRepository,
+	}
+}
+
+func (h *GetListStockRatingQueryHandler) GetListStockRating(getListStockRatingQuery *query.GetListStockRatingQuery) (*query.GetListStockRatingResult, error) {
+	listStockRatings, err := h.stockRatingRepository.GetListStockRating(getListStockRatingQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	var resultList []dto.GetListStockRatingResult
+	for _, stockRating := range *listStockRatings.Results {
+		resultList = append(resultList, dto.GetListStockRatingResult{
+			Ticker:     stockRating.GetTicker(),
+			Company:    stockRating.GetCompany(),
+			Brokerage:  stockRating.GetBrokerage(),
+			Action:     stockRating.GetAction(),
+			RatingFrom: stockRating.GetRatingFrom(),
+			RatingTo:   stockRating.GetRatingTo(),
+			TargetFrom: stockRating.GetTargetFrom(),
+			TargetTo:   stockRating.GetTargetTo(),
+		})
+	}
+
+	return &query.GetListStockRatingResult{
+		Results: &resultList,
+		Metadata: pagination.CreatePaginationMetadata(*getListStockRatingQuery.PageNumber,
+			*getListStockRatingQuery.PageSize, listStockRatings.TotalRecords, int32(len(*listStockRatings.Results))),
+	}, nil
+}
