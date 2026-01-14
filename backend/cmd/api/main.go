87fd7594e8439d20e76321468953afc5c1da2492
Juan F. Abella <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2026-01-14 15:07:57 -0500

    feat(main): add cors configuration, with allowed origins

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jabella1/stock-ratings-service/internal/features/stock/app/query/handler"
 	"github.com/jabella1/stock-ratings-service/internal/features/stock/infra/db/postgres"
 	"github.com/jabella1/stock-ratings-service/internal/features/stock/interface/api/rest"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -35,9 +37,31 @@ func main() {
 	stockRatingByTickerQueryHandler := handler.CreateGetStockRatingByTickerQueryHandler(stockRatingRepository)
 	getListStockRatingQueryHandler := handler.CreateGetListStockRatingQueryHandler(stockRatingRepository)
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: getAllowedOrigins(),
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.OPTIONS},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+	e.Use(middleware.Recover())
+
 	rest.CreateStockRatingController(e, stockRatingByTickerQueryHandler, getListStockRatingQueryHandler)
 
 	if err := e.Start(port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
+}
+
+func getAllowedOrigins() []string {
+	var origins string
+	if origins = os.Getenv("ALLOWED_ORIGINS"); origins == "" {
+		log.Fatal("ALLOWED_ORIGINS is not set")
+	}
+
+	formattedOrigins := strings.Split(origins, ",")
+
+	for i, origin := range formattedOrigins {
+		formattedOrigins[i] = strings.TrimSpace(origin)
+	}
+
+	return formattedOrigins
 }
