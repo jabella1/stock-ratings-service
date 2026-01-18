b245b8a8331b2dadef6a4366cf41242fffa732c6
Juan F. Abella <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2026-01-18 16:19:17 -0500

    feat : added new fields (upside, currentprice) to result in get list stock rating result

--- backend/internal/features/stock/app/dto/get_list_stock_rating_result.go
+++ backend/internal/features/stock/app/dto/get_list_stock_rating_result.go
@@ -1,12 +1,14 @@
 package dto
 
 type GetListStockRatingResult struct {
-	Ticker     string  `json:"ticker"`
-	Company    string  `json:"company"`
-	Brokerage  *string `json:"brokerage"`
-	Action     *string `json:"action"`
-	RatingFrom *string `json:"rating_from"`
-	RatingTo   *string `json:"rating_to"`
-	TargetFrom float64 `json:"target_from"`
-	TargetTo   float64 `json:"target_to"`
+	Ticker       string  `json:"ticker"`
+	Company      string  `json:"company"`
+	Brokerage    *string `json:"brokerage"`
+	Action       *string `json:"action"`
+	RatingFrom   *string `json:"rating_from"`
+	RatingTo     *string `json:"rating_to"`
+	TargetFrom   float64 `json:"target_from"`
+	TargetTo     float64 `json:"target_to"`
+	Upside       float32 `json:"upside"`
+	CurrentPrice float64 `json:"currentPrice"`
 }
