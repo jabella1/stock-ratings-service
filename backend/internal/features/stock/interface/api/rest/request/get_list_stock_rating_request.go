0df1ffb3c6d4f0b0d21e0c602d08ea4497d61114
Juan F. Abella <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2026-01-18 16:25:19 -0500

    feat : add get list stock rating request

---
+++ backend/internal/features/stock/interface/api/rest/request/get_list_stock_rating_request.go
@@ -0,0 +1,12 @@
+package request
+
+type GetListStockRatingRequest struct {
+	Search         *string  `json:"search"`
+	PageSize       *int32   `json:"pageSize"`
+	PageNumber     *int32   `json:"pageNumber"`
+	OrderBy        *string  `json:"orderBy"`
+	OrderDirection *string  `json:"orderDirection"`
+	MinUpside      *float32 `json:"minUpside"`
+	MinPrice       *float64 `json:"minPrice"`
+	MaxPrice       *float64 `json:"maxPrice"`
+}
