03eca4d52edf0d6e514243538108dbb6d073324a
jabella1 <desarrolladorP3@PASSWORDCONSULTING.onmicrosoft.com>
2026-01-20 22:14:05 -0500

    feat : implemented unitOfWork pattern

---
+++ backend/internal/features/stock/infra/db/postgres/unit_of_work.go
@@ -0,0 +1,33 @@
+package postgres
+
+import (
+	"context"
+
+	unitofwork "github.com/jabella1/stock-ratings-service/internal/features/stock/domain/unitOfWork"
+	"github.com/jabella1/stock-ratings-service/internal/features/stock/infra/db/sqlc"
+	"github.com/jackc/pgx/v5"
+)
+
+type UnitOfWork struct {
+	connection *pgx.Conn
+}
+
+func CreateUnitOfWork(connection *pgx.Conn) unitofwork.UnitOfWork {
+	return &UnitOfWork{
+		connection: connection,
+	}
+}
+
+func (unitOfWork *UnitOfWork) BeginTransaction(context context.Context) (unitofwork.DatabaseTransaction, error) {
+	transaction, err := unitOfWork.connection.Begin(context)
+	if err != nil {
+		return nil, err
+	}
+
+	queries := sqlc.New(transaction)
+
+	return &DatabaseTransaction{
+		transaction: transaction,
+		queries:     queries,
+	}, nil
+}
